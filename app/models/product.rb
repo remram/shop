a2c6080eb621d2eb2da110acc9aad11fe932d400
remram <xview28@gmail.com>
2011-04-04 16:46:43 +0200

    Added cart functionality

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,17 @@
 class Product < ActiveRecord::Base
 	default_scope :order => 'title'
+	
+	has_many :line_items
+	before_destroy :ensure_not_referenced_by_any_line_item
+	
+	private	
+	# ensure that there are no line items referencing this product
+	def ensure_not_referenced_by_any_line_item
+		if line_items.count.zero?
+			return true
+		else
+			errors.add(:base, 'Line Items present')
+			return false
+		end
+	end	
 end
